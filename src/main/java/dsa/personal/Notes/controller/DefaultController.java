b55ea9129785cf163c269d7fc1bbfab666640b2c
Deepak <aswal.dsa@gmail.com>
2023-06-18 22:30:23 +0200

    UI elements and controller changes

--- src/main/java/dsa/personal/Notes/controller/DefaultController.java
+++ src/main/java/dsa/personal/Notes/controller/DefaultController.java
@@ -1,7 +1,14 @@
 package dsa.personal.Notes.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import dsa.personal.Notes.entity.Note;
+import dsa.personal.Notes.repository.NoteRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +16,9 @@ import org.slf4j.LoggerFactory;
 @Controller
 public class DefaultController {
     private static final Logger logger = LoggerFactory.getLogger(DefaultController.class);
+
+    private NoteRepository noteRepository;
+
     @GetMapping("/ping")
     public String ping() {
         return "Grüezi !";
@@ -23,4 +33,13 @@ public class DefaultController {
     public String errorPage() {
         return "Wir haben ein Problem.";
     }
+
+    @GetMapping("/notes")
+    public String getAllNotes(Model model) {
+        logger.debug("In getAllNotes");
+        List<Note> notes = new ArrayList<Note>();
+        noteRepository.findAll().forEach(notes::add);
+        model.addAttribute("notes", notes);
+        return "notes";
+    }    
 }
