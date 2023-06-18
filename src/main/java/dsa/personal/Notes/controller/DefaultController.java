7894e3e95a244bbc1bfe838f95875e0de3b46eb4
Deepak <dsa@gmail.com>
2023-06-18 21:57:12 +0000

    UI corrections

--- src/main/java/dsa/personal/Notes/controller/DefaultController.java
+++ src/main/java/dsa/personal/Notes/controller/DefaultController.java
@@ -1,8 +1,11 @@
 package dsa.personal.Notes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import dsa.personal.Notes.entity.Note;
 import dsa.personal.Notes.repository.NoteRepository;
@@ -16,25 +19,36 @@ import org.slf4j.LoggerFactory;
 @Controller
 public class DefaultController {
     private static final Logger logger = LoggerFactory.getLogger(DefaultController.class);
-
+    
+    @Autowired
     private NoteRepository noteRepository;
-
-    @GetMapping("/ping")
-    public String ping() {
-        return "Grüezi !";
-    }
 
     @GetMapping("/")
     public String basePage() {
-        return "Grüezi ! Du bist zu Hause.";
+        return "home";
     }
 
     @GetMapping("/error")
     public String errorPage() {
-        return "Wir haben ein Problem.";
+        return "error";
     }
 
-    @GetMapping("/notes")
+        @GetMapping("/notes")
+    public String getAllNotes(Model model, @RequestParam(required = false) String title) {
+        logger.debug("In getAllNotes");
+        List<Note> notes = new ArrayList<Note>();
+
+        if(title == null) {
+            noteRepository.findAll().forEach(notes::add);
+        }
+        else {
+            noteRepository.findByTitleContainingIgnoreCaseOrDetailsContainingIgnoreCase(title, null).forEach(notes::add);
+        }
+        model.addAttribute("notes", notes);
+        return "notes";
+    }
+    
+    @GetMapping("/notes1")
     public String getAllNotes(Model model) {
         logger.debug("In getAllNotes");
         List<Note> notes = new ArrayList<Note>();
@@ -43,3 +57,4 @@ public class DefaultController {
         return "notes";
     }    
 }
+
